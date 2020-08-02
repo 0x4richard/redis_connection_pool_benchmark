9f6818ca9887f0702f25c58bc02597a1eca06d5e
Richard Hao <xilu@hey.com>
2020-08-02 23:06:51 +0800

    Adjust code order to redis_cache_store_benchmark

--- lib/redis_cache_store_benchmark.rb
+++ lib/redis_cache_store_benchmark.rb
@@ -5,13 +5,13 @@
 REDIS_VALUE = "hellow world".freeze
 
 cache_v1 = ActiveSupport::Cache.lookup_store :redis_cache_store
-pool_v1 = Concurrent::FixedThreadPool.new(5)
-pool_v2 = Concurrent::FixedThreadPool.new(5)
-
 cache_v2 = ActiveSupport::Cache.lookup_store :redis_cache_store, {
   pool_size: 5,
   pool_timeout: 5
 }
+
+pool_v1 = Concurrent::FixedThreadPool.new(5)
+pool_v2 = Concurrent::FixedThreadPool.new(5)
 
 Benchmark.ips do |x|
   x.report "rails default redis_cache_store" do |n|
