9e7a51297398575af9fb452f3e9aab850be740cd
Richard Hao <xilu@hey.com>
2020-08-02 23:00:58 +0800

    Using thread to benchmark redis connection

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 rails runner ./lib/redis_benchmark.rb
 ```
 
+![image](https://user-images.githubusercontent.com/7600503/89125868-f8979500-d513-11ea-9e1d-e83fdad193a3.png)
+
+
 - Rails redis_cache_store benchmark
 
 ```bash
--- lib/redis_benchmark.rb
+++ lib/redis_benchmark.rb
@@ -2,36 +2,43 @@
 
 REDIS_VALUE = "hellow world".freeze
 
+redis_v2 = Redis.new
+redis_v3 = ConnectionPool.new { Redis.new }
+
+pool_v1 = Concurrent::FixedThreadPool.new(5)
+pool_v2 = Concurrent::FixedThreadPool.new(5)
+pool_v3 = Concurrent::FixedThreadPool.new(5)
+
 Benchmark.ips do |x|
   x.report "initialize redis connection every time" do |n|
     n.times do |i|
-      key = "demo_v1_#{i}_time"
-      redis_v1 = Redis.new
-      redis_v1.set key, REDIS_VALUE
-      redis_v1.get key
+      pool_v1.post do
+        key = "demo_v1_#{i}_time"
+        redis_v1 = Redis.new
+        redis_v1.set key, REDIS_VALUE
+        redis_v1.get key
+      end
     end
-  rescue => e
-    p e.message
   end
 
   x.report "initialize single redis connection only" do |n|
-    redis_v2 = Redis.new
-
     n.times do |i|
-      key = "demo_v2_#{i}_time"
-      redis_v2.set key, REDIS_VALUE
-      redis_v2.get key
+      pool_v2.post do
+        key = "demo_v2_#{i}_time"
+        redis_v2.set key, REDIS_VALUE
+        redis_v2.get key
+      end
     end
   end
 
   x.report "initialize redis conection pool" do |n|
-    redis_v3 = ConnectionPool.new { Redis.new }
-
     n.times do |i|
-      key = "demo_v3_#{i}_time"
-      redis_v3.with do |conn|
-        conn.set key, REDIS_VALUE
-        conn.get key
+      pool_v3.post do
+        key = "demo_v3_#{i}_time"
+        redis_v3.with do |conn|
+          conn.set key, REDIS_VALUE
+          conn.get key
+        end
       end
     end
   end
